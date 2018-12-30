c99f0a105e4c95ccc12e94b273ea6079df693f35
muhammadyusufalfaqih <45525489+muhammadyusufalfaqih@users.noreply.github.com>
2018-12-30 15:05:45 -0800

    Add files via upload

---
+++ gaji karyawan.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+int gaji;
+int jam;
+char nama;
+cout<<"PROGRAM INPUT GAJI KARYAWAN\n"<<endl;
+cout<<"gaji karyawan rp.12000 perjam dalam kerja 2 jam"<<endl;
+cout<<"masukan nama karyawan= ";
+cin>>nama;
+
+    gaji=2*12000;
+    cout<<"total gaji anda= ";
+    cout<<"rp."<<gaji<<endl;
+    std::cout<<" \n";
+    std::cout<<"nama  : muhammad yusuf alfaqih \n";
+    std::cout<<"nim   : 311810239";
+    return 0;
+}
